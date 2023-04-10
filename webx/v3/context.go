7f65dc3d4250a83018d828b0eb261cf271ebd02f
sjxiang <1535484943@qq.com>
2023-04-11 01:47:29 +0800

    webx context 处理输入输出 over

--- webx/v3/context.go
+++ webx/v3/context.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+	"sync"
 )
 
 // 代表上下文
@@ -67,6 +68,24 @@ func (ctx *Context) QueryValue(key string) (string, error) {
 	return vals[0], nil 
 }
 
+func (ctx *Context) QueryValueV1(key string) StringValue {
+	if ctx.cacheQueryValue == nil {
+		ctx.cacheQueryValue = ctx.Req.URL.Query()
+	}
+
+	vals, ok := ctx.cacheQueryValue[key]
+	if !ok || len(vals) == 0 {
+		return StringValue{err: errors.New("webx: key not found")}
+	}
+
+	return StringValue{val: vals[0]}
+}
+
+
+func (ctx *Context) QueryValueAsInt64V1(key string) (int64, error) {
+	return ctx.QueryValueV1(key).ToInt64()
+}
+
 
 func (ctx *Context) QueryValueAsInt64(key string) (int64, error) {
 	val, err := ctx.QueryValue(key)
@@ -90,7 +109,6 @@ func (ctx *Context) pathValue(key string) (string, error) {
 }
 
 
-
 // 通用的处理方案（解决转换指定类型）
 type StringValue struct {
 	val string
@@ -99,5 +117,50 @@ type StringValue struct {
 
 func (s StringValue) String() (string, error) {
 	return s.val, s.err
+}
+
+
+func (s StringValue) ToInt64() (int64, error) {
+	if s.err != nil {
+		return 0, s.err
+	}
+
+	return strconv.ParseInt(s.val, 10 , 64)
+}
+
+
+
+
+// JSON 响应
+func (ctx *Context) RespJSON(code int, val any) error {
+	bs, err := json.Marshal(val)
+	if err != nil {
+		return err
+	}
+	ctx.Resp.WriteHeader(code)
+	_, err = ctx.Resp.Write(bs)
+	return err
+}
+
+
+// 设置 cookie
+func (ctx *Context) SetCookie(cookie *http.Cookie) {
+	http.SetCookie(ctx.Resp, cookie)
+}
+
+
+
+// 线程安全
+type SaveContext struct {
+	c Context
+	l sync.Mutex
+}
+
+// 装饰器
+func (ctx *SaveContext) RespJSON(code int, val any) error {
+	ctx.l.Lock()
+	defer ctx.l.Unlock()
+
+	return ctx.c.RespJSON(code, val)
 }
 
